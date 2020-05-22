24ca1d5589ba3e941fe7a8ae5be09f33756141aa
SchatzM <bri@npass.us>
2020-05-22 17:54:41 +0200

    Añadida lógica

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,35 @@
 'use strict';
+
+/**
+ * Práctica 08
+ * Construir Objetos Automáticamente
+ * Brian Passos
+ */
+
+var coches = {}; // Variable global que almacenará las instancias de vehículo
+
+// Constructor
+function vehiculo (ID, modelo, color, estado) {
+	this.ID = ID;
+	this.modelo = modelo;
+	this.color = color;
+	this.estado = estado;
+}
+
+function generarCoches (cantidad) {
+	let marcasDisponibles = ['Mazda', 'Toyota', 'Nissan', 'Peugeot', 'Audi', 'BMW', 'Ford'],
+		coloresDisponibles = ['Rojo', 'Verde', 'Azul', 'Negro', 'Gris', 'Blanco'],
+		estadosPosibles = ['Nuevo', 'Segunda mano', 'No disponible'],
+		cochesAConstruir = cantidad || 10;
+
+	for (let i = 0; i < cochesAConstruir; i++) {
+		coches = 	new vehiculo (i+1,
+					marcasDisponibles[Math.floor(Math.random() * marcasDisponibles.length)],
+					coloresDisponibles[Math.floor(Math.random() * coloresDisponibles.length)],
+					estadosPosibles[Math.floor(Math.random() * estadosPosibles.length)]);
+
+		console.info (coches);
+	}
+}
+
+generarCoches ();
